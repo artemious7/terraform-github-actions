ba029ef5d87c6184693ff81d3d75d68b2b964b6c
Artem <16724889+artemious7@users.noreply.github.com>
2024-10-09 23:45:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
     storage_account_name = "tfstate85581740"
     container_name       = "tfstate2"
     key                  = "terraform.tfstate"
-    subscription_id      = var.subscription_id
+    subscription_id      = "fc0369ac-8fc2-44b4-8846-76d5fc8420eb"
     use_oidc             = true
   }
 }
